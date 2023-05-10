a91ebd0b825a99328a3edeab512395aa8765595a
ihsan YILMAZ <arkihekQA@gmail.com>
2023-05-10 19:22:39 +0300

    Day 19

--- src/day18/P09.java
+++ src/day18/P09.java
@@ -2,7 +2,7 @@ package day18;
 
 public class P09 {
     /*
-    Verilen pozitif bir n tamsayisini alarak, bize ilk n tane tane Fibonacci
-    sayisini bir list olarak donduren bir method olusturun.
+       Verilen pozitif bir n tamsayisini alarak, bize ilk n tane tane Fibonacci
+       sayisini bir list olarak donduren bir method olusturun.
      */
 }
